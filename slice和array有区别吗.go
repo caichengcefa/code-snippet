ae3f6b6436be3fc499d5e5560430fcbeb840931e
caichengcefa <yaozhongfa2010@126.com>
2023-01-15 10:41:48 +0800

    Update slice和array有区别吗.go

--- slice和array有区别吗.go
+++ slice和array有区别吗.go
@@ -4,16 +4,15 @@ import "fmt"
 
 func main() {
 
-	var a1 [3]int // 不初始化默认0值   bool类型不初始化默认false  字符串:""
-	var a2 [4]int
-	fmt.Printf("a1:%T  a2:%T\n", a1, a2)
-	fmt.Println(a1[0])
-  // 打印结果：
-  // a1:[3]int  a2:[4]int
-	// 0
-  
-	a3 := make([]int, 4, 6)
-	fmt.Printf("a3=%T a3=%v, len(a3)=%d, cap(a3)=%d\n", a3, a3, len(a3), cap(a3))
-  // 打印结果：
-	// a3=[]int a3=[0 0 0 0], len(a3)=4, cap(a3)=6
+	var a1 [3]int                       // 不初始化默认0值   bool类型不初始化默认false  字符串:""
+	fmt.Printf("a1=%T a1=%v\n", a1, a1) //a1=[3]int a1=[0 0 0]
+	fmt.Println(a1[0])                  //0
+
+	a2 := make([]int, 3, 4)
+	fmt.Printf("a2=%T a2=%v, len(a2)=%d, cap(a2)=%d\n", a2, a2, len(a2), cap(a2)) //a2=[]int a2=[0 0 0], len(a2)=3, cap(a2)=4
+
+	a3 := [...]int{1, 3, 5, 7, 9}
+	s1 := a3[0:3]
+	fmt.Printf("s1=%T s1=%v\n", s1, s1) //s1=[]int s1=[1 3 5]
+
 }
